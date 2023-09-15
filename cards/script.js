c0d44d2015a9cea28740c582aa8d92c8ae7893be
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-09-15 12:46:26 +0000

    commit

--- cards/script.js
+++ cards/script.js
@@ -33,4 +33,41 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream1").addClass('highlight_stream');
     });
+
+    $("#stream1_btn").on(function() {
+        $(".stream1").fadeTo(1000, 0.5);
+
+        $(".stream1").fadeToggle();
+        $(".stream1").fadeToggle(1000);
+        
+        $(".stream1").fadeOut();
+        $(".stream1").fadeOut(1000);
+
+        $(".stream1").fadeIn();
+        $(".stream1").fadeIn(1000);
+        
+        $(".stream1").slideToggle();
+        $(".stream1").slideToggle(1000);
+
+        $(".stream1").slideUp();
+        $(".stream1").slideUp(1000);
+
+        $(".stream1").slideDown();
+        $(".stream1").slideDown(1000);
+
+        $(".stream1").toggle();
+        $(".stream1").toggle(1000);
+        
+        $(".stream1").hide();
+        $(".stream1").hide('slow');
+        $(".stream1").hide('medium');
+        $(".stream1").hide('fast');
+        $(".stream1").hide(1000);
+        
+        $(".stream1").show('');
+        $(".stream1").show('slow');
+        $(".stream1").show('medium');
+        $(".stream1").show('fast');
+        $(".stream1").show(1000);
+    });
 }); 
